5589799448d7e151d14a96c324b2e5d111a24edb
alexanderserzhanov <aserzhanov85@mail.ru>
2024-09-07 20:02:22 +0300

    Initial commit.

---
+++ src/main/java/ru/sbrf/transfer/docker/docker_lab/DockerLabApplication.java
@@ -0,0 +1,13 @@
+package ru.sbrf.transfer.docker.docker_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sbrf/transfer/docker/docker_lab/DockerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbrf.transfer.docker.docker_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
